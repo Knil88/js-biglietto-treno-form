51b8e7ad3eaa3565e739a9479653f1b94f1ef5c5
Yari <narducciyarigmail.com>
2022-11-08 18:36:19 +0100

    ultimo modifica

--- js/script.js
+++ js/script.js
@@ -1,16 +1,41 @@
 //Generatore Biglietto Treno
 let btn = document.getElementById("generate");
+let btnReset = document.getElementById("reset")
 
 btn.addEventListener("click",
 
-function (){
+function (event)
+{
+    event.preventDefault()
     let name = document.getElementById ("inputname").value;
     let distance = document.getElementById ("inputdistance").value;
     let age = document.getElementById ("inputage").value;
     const kmPrice = 0.21 ;
     console.log(kmPrice);
     let price = kmPrice * distance;
-    console.log(price);
+    console.log(price,"€");
+    if(age == "minorenne"){
+        let discount = price * 20 / 100;
+
+        let salePrice =(price - discount).toFixed(2);
+
+        console.log(salePrice,"€");
+         }
+         else if(age == "over65"){
+            let overDiscount = price * 40 / 100;
+            let overPrice = (price - overDiscount).toFixed(2);
+
+            console.log(overPrice,"€").toFixed(2);
+         }
+         else(age == maggiorenne){
+            price = price;
+         }
+         name = document.getElementById("ticketname")
+         `${name}`
+
+         
+
+        
 
 
 }
